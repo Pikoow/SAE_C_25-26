c0b6a46f086637a17bba76b114766b63e7ac8967
Vieil_Arbre <161038674+YannChesnel@users.noreply.github.com>
2025-10-15 14:47:36 +0200

    Update plot_avg_track_nb_per_years.py
    j'ai oublié de tout mettre en anglais lol

--- Python/Graphs/plot_avg_track_nb_per_years.py
+++ Python/Graphs/plot_avg_track_nb_per_years.py
@@ -4,22 +4,22 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("new_albums.csv", sep=",", on_bad_lines="warn")
 
-# on ajoute une colonne fictive "album year" pour récupérer
-# uniquement l'année de date de sortie des albums
+# We are creating a new column "album year" that claims
+# the year of the albums release date
 df["album_year"] = pd.to_datetime(df["album_date_released"]).dt.year
 
-# Pour retirer toutes les années vide
+# To clean all the years without data (if there are)
 df = df.dropna(subset=["album_year", "album_tracks"])
 
-# Moyenne des tracks par années
+# The average tracks per album per year
 tracks_by_year = df.groupby("album_year")["album_tracks"].mean()
 # print(tracks_by_year)
 
-# Graphique
+# The graph
 plt.figure(figsize=(13,5))
 plt.plot(tracks_by_year.index, tracks_by_year.values, marker="o")
-plt.title("Évolution du nombre moyen de morceaux par album")
-plt.xlabel("Année de sortie")
-plt.ylabel("Nombre moyen de morceaux")
+plt.title("Evolution of the average number of tracks in albums through the years")
+plt.xlabel("Years where an album got published")
+plt.ylabel("Average track number per albums")
 plt.grid(True, linestyle="--", alpha=0.6)
 plt.show()
